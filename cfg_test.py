8f27be080ffd2f1707884cc43415b8377b48ea7e
Andrew <milichab@gmail.com>
2016-12-12 22:36:30 -0800

    first set test

---
+++ cfg_test.py
@@ -0,0 +1,12 @@
+from nltk.parse.generate import generate, demo_grammar
+from nltk import CFG
+
+grammar = CFG.fromstring("""
+	S -> X | '{' Z '}'
+	Z -> X ',' Z | X
+	X -> Y | X 'U' X 
+	Y -> '{}' | 'N' 
+	""")
+
+for sentence in generate(grammar, depth=6, n=15):
+	print(' '.join(sentence))
