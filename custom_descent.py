1d97a9d859e464708b7cc608101a1abec1bf8775
Andrew <milichab@gmail.com>
2016-12-15 16:59:03 -0500

    custom recursive descent

---
+++ custom_descent.py
@@ -0,0 +1,18 @@
+
+
+class Terminal: 
+	def __init__(terminal): 
+		self.terminal = terminal
+
+class Nonterminal: 
+	def __init__(letter, replacements): 
+		self.letter = letter 
+		self.replacements = replacements
+
+def can_replace(string, nontermial): 
+	# recursive backtrack 
+	return False 
+
+if __name__ == '__main__': 
+	# make nonterminal 
+	# make string 
