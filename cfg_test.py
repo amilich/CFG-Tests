b4cf07379cc347d27a20f3a894bc42af83328d21
Andrew <milichab@gmail.com>
2016-12-15 16:56:37 -0500

    comment

--- cfg_test.py
+++ cfg_test.py
@@ -1,3 +1,7 @@
+"""
+103 
+"""
+
 from nltk.parse.generate import generate, demo_grammar
 from nltk import CFG
 
@@ -5,8 +9,8 @@ grammar = CFG.fromstring("""
 	S -> X | '{' Z '}'
 	Z -> X ',' Z | X
 	X -> Y | X 'U' X 
-	Y -> '{}' | 'N' 
+	Y -> '{}' | 'N' | 'R'
 	""")
 
-for sentence in generate(grammar, depth=6, n=15):
+for sentence in generate(grammar, depth=6, n=200):
 	print(' '.join(sentence))
